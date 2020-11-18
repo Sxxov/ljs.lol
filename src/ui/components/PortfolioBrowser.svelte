804c9f156afd4e6bb154ee340fb2ac05c7c056ed
Sxxov <sxxovnetwork@gmail.com>
2020-11-18 20:01:21 +0800

    Implement image buttons

--- src/ui/components/PortfolioBrowser.svelte
+++ src/ui/components/PortfolioBrowser.svelte
@@ -1,13 +1,17 @@
 <script>
 	import { writable } from 'svelte/store';
+	// import { dropIn, dropOut } from '../../core/transitioner';
 	import { navigatorAppBarExpandedHeight } from '../../resources/stores';
 
 	import { CSSUtility, WindowUtility } from '../../resources/utilities';
 
 	import Button from '../blocks/Button.svelte';
+	import Image from '../blocks/Image.svelte';
+	import Scene from '../blocks/Scene.svelte';
 
 	/** @type {Record<string, () => void>} */
 	export let keyAndCallbackMap;
+	export let keyAndPreviewImageMap = [];
 
 	const keys = Object.keys(keyAndCallbackMap);
 	const callbacks = Object.values(keyAndCallbackMap);
@@ -18,34 +22,64 @@
 	$: componentHeightWritable.set(inner.height - $navigatorAppBarExpandedHeight);
 </script>
 
-<component
-	style='
-		--height: {CSSUtility.parse($componentHeightWritable)}
-	'
+<Scene
+	isPadded={false}
+	isInAnimated={true}
+	isOutAnimated={true}
+	width='100%'
 >
-	{#each keys as key, i}
-		<container>
-			<Button
-				icon='arrow_forward'
-				height='100%'
-				width='100%'
-				on:click={callbacks[i]}
-				roundness='0'
+	<container
+		class='all'
+		style='
+			--height: {CSSUtility.parse($componentHeightWritable)}
+		'
+	>
+		{#each keys as key, i}
+			<container
+				class='button'
 			>
-				<heading>
-					{key}
-				</heading>
-			</Button>
-		</container>
-	{/each}
-</component>
+				<Button
+					height='100%'
+					width='100%'
+					on:click={callbacks[i]}
+					roundness='0'
+					padding='0'
+					hoverColour={keyAndPreviewImageMap[key] ? '--colour-background-primary' : '--colour-accent-secondary'}
+					backgroundColour={keyAndPreviewImageMap[key] ? 'transparent' : '--colour-accent-primary'}
+				>
+					<icon 
+						class={keyAndPreviewImageMap[key] ? 'hover-off' : ''} 
+						slot='icon'
+					>
+						arrow_forward
+					</icon>
+					<heading
+						class={keyAndPreviewImageMap[key] ? 'hover-off' : ''} 
+					>
+						{key}
+					</heading>
+					<container
+						class='background'
+						slot='background'
+					>
+						{#if keyAndPreviewImageMap[key]}
+							<Image 
+								src={keyAndPreviewImageMap[key]}
+							/>
+						{/if}
+					</container>
+				</Button>
+			</container>
+		{/each}
+	</container>
+</Scene>
 
 <style>
-	component {
+	container.all {
 		display: grid;
 		grid-auto-flow: column;
 		grid-auto-columns: 1fr;
-		grid-template-rows: 1fr;
+		grid-auto-rows: 1fr;
 
 		height: var(--height);
 		width: 100%;
@@ -53,6 +87,37 @@
 		/* gap: var(--padding); */
 		/* padding: var(--padding); */
 		box-sizing: border-box;
+	}
+
+	@media only screen and (max-width: 840px) {
+		container.all {
+			grid-auto-flow: row;
+		}
+	}
+
+	container.background {
+		height: 100%;
+		width: 100%;
+
+		position: absolute;
+		top: 0;
+		left: 0;
+
+		z-index: -1;
+
+		opacity: 1;
+	}
+
+	heading.hover-off, 
+	icon.hover-off {
+		opacity: 0;
+
+		transition: opacity 0.2s var(--ease-slow-slow);
+	}
+
+	container.button:hover heading.hover-off, 
+	container.button:hover icon.hover-off {
+		opacity: 1;
 	}
 </style>
 
