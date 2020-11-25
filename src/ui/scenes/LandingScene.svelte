c0d7ed8870374cc4f0aa2abe0c12a6ff28bceac2
Sxxov <sxxovnetwork@gmail.com>
2020-11-26 00:49:20 +0800

    Implement BackgroundHeading

--- src/ui/scenes/LandingScene.svelte
+++ src/ui/scenes/LandingScene.svelte
@@ -5,11 +5,12 @@
 	import contacts from '../../resources/contacts';
 	import Lottie from '../blocks/Lottie.svelte';
 	import Terminal from '../components/Terminal.svelte';
-	import { WindowUtility, $ as S } from '../../resources/utilities';
+	import { WindowUtility } from '../../resources/utilities';
 	import { navigatorAppBarExpandedHeight } from '../../resources/stores';
 	import Collage from '../components/Collage.svelte';
 	import CallToAction from '../components/CallToAction.svelte';
 	import Footer from '../components/Footer.svelte';
+	import BackgroundHeading from '../components/BackgroundHeading.svelte';
 
 	const { innerWritable } = WindowUtility;
 	$: inner = $innerWritable;
@@ -57,9 +58,75 @@
 	isInAnimated={true}
 	isOutAnimated={true}
 >
-	<Terminal
-		heightWritable={terminalHeightWritable}
-	/>
+	<container
+		class='background-heading'
+	>
+		<BackgroundHeading
+			textAlign='left'
+			index=0
+		>
+			<span slot='fill'>
+				hell
+				o,
+			</span>
+			<span slot='outline'>
+				hey...
+				yo!
+			</span>
+		</BackgroundHeading>
+		<BackgroundHeading
+			textAlign='right'
+			index=1
+		>
+			<span slot='fill'>
+				what
+				up?
+			</span>
+			<span slot='outline'>
+				yeah,
+				you!
+			</span>
+		</BackgroundHeading>
+		<BackgroundHeading
+			textAlign='left'
+			index=2
+		>
+			<span slot='fill'>
+				<br>
+				{@html 
+					`would
+					you
+					like
+					to
+					know
+					me
+					?`
+				}
+			</span>
+		</BackgroundHeading>
+		<BackgroundHeading
+			textAlign='right'
+			index=3
+		>
+			<span slot='outline'>
+				i
+				do
+				hope
+				you'll
+				stick
+				around
+				:*
+			</span>
+		</BackgroundHeading>
+	</container>
+	
+	<container
+		class='terminal'
+	>
+		<Terminal
+			heightWritable={terminalHeightWritable}
+		/>
+	</container>
 
 	<container
 		class='collage'
@@ -100,6 +167,15 @@
 
 	container.collage {
 		width: 100%;
+
+		z-index: 0;
+	}
+
+	container.terminal {
+		height: 100%;
+		width: 100%;
+
+		z-index: 2;
 	}
 	/* container.dialog {
 		display: flex;
@@ -113,4 +189,15 @@
 		font-size: 1.5rem;
 		width: 3rem;
 	} */
+
+	container.background-heading {
+		z-index: 1;
+		/* top: 0; */
+
+		position: absolute;
+
+		width: 100%;
+
+		mix-blend-mode: difference;
+	}
 </style>
