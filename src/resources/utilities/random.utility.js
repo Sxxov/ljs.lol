e992b9b12ca4e31cfa45c843c3198a675386bd35
Sxxov <sxxovnetwork@gmail.com>
2020-11-28 04:14:24 +0800

    Implement lengthed int & string random methods

--- src/resources/utilities/random.utility.js
+++ src/resources/utilities/random.utility.js
@@ -1,7 +1,17 @@
 export class RandomUtility {
-    static int() {
-        return Math.floor(Math.min(Math.random()
-            * (Number.MAX_SAFE_INTEGER / 2), Number.MAX_SAFE_INTEGER));
+    static int(length = 16) {
+        return Number(new Array(length)
+            .fill(null)
+            .map(() => String(Math.floor(Math.min(Math.random() * 10, 9))))
+            .join(''));
+    }
+    static string(length = 16, charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789') {
+        let result = '';
+        const { length: charsetLength } = charset;
+        for (let i = 0; i < length; i++) {
+            result += charset.charAt(Math.floor(Math.random() * charsetLength));
+        }
+        return result;
     }
 }
 //# sourceMappingURL=random.utility.js.map
--- src/resources/utilities/random.utility.ts
+++ src/resources/utilities/random.utility.ts
@@ -1,11 +1,33 @@
 export class RandomUtility {
-	public static int(): number {
-		return Math.floor(
-			Math.min(
-				Math.random()
-				* (Number.MAX_SAFE_INTEGER / 2),
-				Number.MAX_SAFE_INTEGER,
-			),
+	public static int(
+		length = 16,
+	): number {
+		return Number(
+			new Array(length)
+				.fill(null)
+				.map(() => String(
+					Math.floor(
+						Math.min(
+							Math.random() * 10,
+							9,
+						),
+					),
+				))
+				.join(''),
 		);
+	}
+
+	public static string(
+		length = 16,
+		charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
+	): string {
+		let result = '';
+		const { length: charsetLength } = charset;
+
+		for (let i = 0; i < length; i++) {
+			result += charset.charAt(Math.floor(Math.random() * charsetLength));
+		}
+
+		return result;
 	}
 }
